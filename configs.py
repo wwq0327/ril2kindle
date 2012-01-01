88a908cca3897f3f43186bf7d105930fc968539c
wwq0327 <wwq0327@gmail.com>
2012-01-01 09:25:16 +0800

    first

---
+++ configs.py
@@ -0,0 +1,6 @@
+# Rename this file to test_configs.py and add your RIL credentials.
+
+RIL_APIKEY = '<KEY>'
+RIL_USERNAME = '<username>'
+RIL_PASSWORD = '<password>'
+
---
+++ test_configs.py
@@ -0,0 +1,6 @@
+# Rename this file to test_configs.py and add your RIL credentials.
+
+RIL_APIKEY = '9a8TeVXOg7fv3J3a2dd5dtsBf3p4m3e5'
+RIL_USERNAME = 'wwq0327'
+RIL_PASSWORD = 'qq123456'
+
